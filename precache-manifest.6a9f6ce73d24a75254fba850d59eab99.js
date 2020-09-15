8cfc18abf3c84781a22fec4faa56725f6ca5837b
John Nielsen <johnhayesnielsen@gmail.com>
2020-09-15 14:23:51 -0600

    Updates

---
+++ precache-manifest.6a9f6ce73d24a75254fba850d59eab99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1af181766caeb530c3ab14d7b50881",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f0af13719947fa4de541",
+    "url": "/react-portfolio/static/css/main.f3215123.chunk.css"
+  },
+  {
+    "revision": "06624b9b34f586f18a98",
+    "url": "/react-portfolio/static/js/2.c86ee92f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.c86ee92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0af13719947fa4de541",
+    "url": "/react-portfolio/static/js/main.78580c77.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "cd74df75024036f2c42b7c22efae6d70",
+    "url": "/react-portfolio/static/media/bright-headshot-small.cd74df75.jpg"
+  },
+  {
+    "revision": "99ae7ed88779d7c36566faa6bcaeda9e",
+    "url": "/react-portfolio/static/media/wood-background.99ae7ed8.jpg"
+  }
+]);
