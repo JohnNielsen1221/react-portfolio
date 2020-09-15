4ae08d1aafbbe6a0fad27ea9f7c86c321d68a1cd
John Nielsen <johnhayesnielsen@gmail.com>
2020-09-15 01:42:05 -0600

    made responsive

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,14 +39,14 @@ function ContactForm() {
             <h1 >Hello... Is it me you're looking for?</h1>
             <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor='name'>Name:</label>
+                    <label className='form-name' htmlFor='name'>Name:</label>
                     <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
-                    <label htmlFor='email'>Email address:</label>
+                    <label className='form-email' htmlFor='email'>Email address:</label>
                     <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
-                <div>
+                <div className='form-message'>
                     <label htmlFor='message'>Message:</label>
                     <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
                 </div>
@@ -57,7 +57,7 @@ function ContactForm() {
                 )}
                 <button data-testid="buttontag" type='submit'>Submit</button>
             </form>
-            <h2> You may also reach me at 
+            <h2 className='email-contact'> You may also reach me at 
                 johnhayesnielsen@gmail.com
             </h2>
         </section>
